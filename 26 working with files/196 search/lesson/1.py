e6b5901e437e650fc7f434e728734783fd34c6a8
190101101 <190101101@mail.ru>
2023-01-17 20:51:52 +0400

    3.1

--- 26 working with files/196 search/lesson/1.py
+++ 26 working with files/196 search/lesson/1.py
@@ -1,4 +1,8 @@
 import os
 from pathlib import Path
 
-cwd = os.getcwd()
+# find = 'C:\\Users\\190101101\\dev\\python\\base\\00 bin\\docs'
+find = 'C:\\Users\\190101101\\dev\\python'
+
+for i in os.listdir(find):
+	print(i)
