15fd9aeab3864564d531745d043dd0674d41e7a9
190101101 <190101101@mail.ru>
2023-01-18 08:13:40 +0400

    3.3

--- 26 working with files/198 zip/lesson/2.py
+++ 26 working with files/198 zip/lesson/2.py
@@ -1,11 +1,9 @@
+import shutil
 import zipfile
 import os
 
 p1 = zipfile.ZipFile('files/2.zip', 'r')
 
-for i in p1.filelist:
-	print(i)
 
 
-p1.extractall()	
-p1.close()
+shutil.make_archive('files/newfiles', 'zip', 'files')
